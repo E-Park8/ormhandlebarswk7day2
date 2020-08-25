d9b9ad003b3bfa1fe96c1d77a70c9bda5866f573
Eric Park <epark0995@gmail.com>
2020-08-25 10:26:12 -0700

    functional handlebars

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const { join } = require('path')
+
+const app = express()
+
+app.use(express.static(join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.engine('.hbs', require('express-handlebars')({ extname: '.hbs' }))
+app.set('view engine', '.hbs')
+
+app.get('/bio', (req, res) => {
+    res.render('bio')
+})
+
+app.get('*', (req, res) =>{
+    res.render('kenny')
+})
+
+
+app.listen(3000)
